5880589eb5e9ce0208eb2f73e487ab98165a2885
Johnny <13565121+jpixy@users.noreply.github.com>
2025-10-09 10:45:31 +0800

    Update init_user.sh

--- init_user.sh
+++ init_user.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
 
 sudo useradd -m -d /localhome/keystone -s /bin/bash keystone
-sudo usermod -aG sudo keystone
-sudo usermod -aG wheel keystone
-sudo usermod -aG adm keystone
-sudo usermod -aG admin keystone
-echo "changeit" | sudo passwd --stdin $USER
-echo -e '\n%wheel ALL=(ALL:ALL) NOPASSWD:ALL\n%admin ALL=(ALL:ALL) NOPASSWD:ALL\n%sudo ALL=(ALL:ALL) NOPASSWD:ALL' | sudo tee -a /etc/sudoers.d/nopasswd-groups && sudo visudo -c
+
+for group in wheel adm; do
+    sudo usermod -aG $group keystone 2>/dev/null && echo "Added to $group" || echo "Group $group not found"
+done
+
+echo "keystone:changeit" | sudo chpasswd
+
+sudo chmod 0440 /etc/sudoers.d/* 2>/dev/null || true
+
+{
+    [[ $(getent group wheel) ]] && echo '%wheel ALL=(ALL:ALL) NOPASSWD:ALL'
+    [[ $(getent group admin) ]] && echo '%admin ALL=(ALL:ALL) NOPASSWD:ALL' 
+    [[ $(getent group sudo) ]] && echo '%sudo ALL=(ALL:ALL) NOPASSWD:ALL'
+} | sudo tee /etc/sudoers.d/nopasswd-groups
+
+sudo chmod 0440 /etc/sudoers.d/nopasswd-groups
+sudo visudo -c
